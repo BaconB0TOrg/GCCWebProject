2abbbaf0f3a0608f82d15c60b9d77c377567f05d
Relicjamin-jv <campbellcl19@gcc.edu>
2022-11-13 20:10:28 -0500

    port to make_server has been added to allow servers to be made on different ports

--- docker/mcdocker.py
+++ docker/mcdocker.py
@@ -1,9 +1,9 @@
 import docker
 
 
-def make_server(return_container=False):
+def make_server(return_container=False, port=25565):
     client = docker.from_env() 
-    mc_server_container = client.containers.run(image="itzg/minecraft-server", detach=True, ports={'25565':'25565'}, name="mc", environment=["EULA=True"])
+    mc_server_container = client.containers.run(image="itzg/minecraft-server", detach=True, ports={f"{port}":f"{port}"}, name="mc", environment=["EULA=True"])
     if return_container:
         return mc_server_container
     else:
@@ -41,7 +41,7 @@ def start_docker(container_id=None):
 
 
 if __name__ == "__main__":
-    #make_server()
-    run_docker_mc_command("mc", "/banlist players")
+    make_server()
+    #run_docker_mc_command("mc", "/banlist players")
     #stop_docker("mc")  
     #start_docker("mc")  
