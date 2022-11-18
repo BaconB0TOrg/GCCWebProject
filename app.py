3a77d24f9ed6211da450146c901b0d1685d8ad57
homan-jonathan <homanjp20@gcc.edu>
2022-11-17 19:14:37 -0500

    adds basic login and register functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import models
 from flask import Flask
+from flask import render_template, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 
 scriptdir = os.path.abspath(os.path.dirname(__file__))
@@ -13,6 +14,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbpath}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+@app.get('/welcome/')
+def welcome():
+    return render_template("home.html")
+
+@app.get('/login/')
+def get_login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
+@app.get('/register/')
+def get_register():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
+
+@app.post("/login/")
+def post_login():
+  form = LoginForm()
+  if form.validate():
+    return redirect(url_for('welcome'))
+  else:
+    for field,error in form.errors.items():
+      flash(f"{field}: {error}")
+    return redirect(url_for('get_login'))
+
+@app.post('/register/')
+def post_register():
+    form = RegisterForm()
+    if form.validate():
+        return redirect(url_for('welcome'))
+    else:
+        # flash error messages for all validation problems 
+        for field,error in form.errors.items():
+            flash(f"{field}: {error}")
+        return redirect(url_for('get_register'))
 
 with app.app_context():
   print(dbpath)
