83c96432d1cc05ee19f0a68db8201ab3920365f5
@EthanStyle <brownev19@gcc.edu>
2022-11-29 21:44:25 -0500

    Update app.py
    Login authorization and /site-map/ route to see what routes we have currently implemented.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import mc_lib.mcdocker as mcdocker
 import os, models
-from flask import Flask
+from flask import Flask, escape
 from flask import render_template, url_for, redirect, flash, jsonify, request, session
 from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import sha256_crypt
 
 scriptdir = os.path.abspath(os.path.dirname(__file__))
 dbpath = os.path.join(scriptdir, 'server_hosting.sqlite3')
@@ -15,6 +16,29 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbpath}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+#####
+# Utilities
+####
+
+def flash_form_errors(form):
+  for field,error in form.errors.items():
+    flash(f"{field}: {error}")
+
+#########################
+# Route Handlers        #
+#########################
+
+@app.route("/site-map/")
+def site_map():
+  links = []
+  for rule in app.url_map.iter_rules():
+    if "GET" in rule.methods:
+      rule = escape(rule.rule)
+      links.append(f"<a href={rule}>{rule}</a>")
+  # links is now a list of url's with their parameters
+  linksString = '<br>'.join(links)
+  return linksString
 
 @app.get('/')
 def redirect_to_welcome():
@@ -31,20 +55,26 @@ def get_login():
 
 @app.post("/login/")
 def post_login():
+  # TODO: Don't allow logged in users to nav here
   form = LoginForm()
   if form.validate():
     # get user if exists and go from there
-    email = form.username.data
-    pw = form.password.data
-    # TODO: auth user
-    user = User.query.filter_by(email=email, password=pw).first()
+    username = form.username.data
+    user = User.query.filter_by(username=username).first()
+
     if user == None:
-      flash("Incorrect username and password combination!")
+      flash("Incorrect username and/or password combination!")
       return redirect(url_for('get_login'))
+    print(user.password)
+
+    valid = sha256_crypt.verify(str(form.password.data), user.password)
+    if not valid:
+      flash("Incorrect username and/or password combination!")
+      return redirect(url_for('get_login'))
+    flash(f"Welcome back {user.username}")
     return redirect(url_for('welcome'))
   else:
-    for field,error in form.errors.items():
-      flash(f"{field}: {error}")
+    flash_form_errors(form)
     return redirect(url_for('get_login'))
 
 @app.get('/register/')
@@ -56,20 +86,35 @@ def get_register():
 def post_register():
     form = RegisterForm()
     if form.validate():
-        # TODO: save to db
-        pw = form.password.data
         email = form.email.data
         uname = form.username.data
+        checkUsers = User.query.filter_by(email=email).all()
+        if len(checkUsers) > 0:
+          # email already exists.
+          flash("That email is already in use!")
+          return redirect(url_for('get_regiser'))
+        checkUsers = User.query.filter_by(username=uname).all()
+        if len(checkUsers) > 0:
+          # username already exists.
+          flash("That username is taken!")
+          return redirect(url_for('get_regiser'))
+        # encrypt password.
+        pw = sha256_crypt.encrypt(str(form.password.data))
         user = User(password=pw, email=email, username=uname)
+
         db.session.add(user)
         db.session.commit()
+
         user = User.query.filter_by(email=email).first()
+
+        session['logged-in'] = True
         session['user-email'] = user.email
-        return redirect('/server/')
+        flash(f"Welcome {uname}!")
+
+        return redirect(url_for('welcome'))
     else:
         # flash error messages for all validation problems 
-        for field,error in form.errors.items():
-            flash(f"{field}: {error}")
+        flash_form_errors(form)
         return redirect(url_for('get_register'))
 
 @app.get('/server/<int:server_id>/')
@@ -90,11 +135,12 @@ def list_server():
   sf = ServerForm()
   # do something to make sure users can't edit their token
   # and be logged in to someone else's account.
-  email=session.get('user-email')
-  if email == None:
+  loggedIn=session.get('logged-in')
+  if not loggedIn:
     flash('You have to be logged in to access that page!')
     return redirect('/login/')
-
+  
+  email=session.get('user-email')
   user = User.query.filter_by(email=email).first()
   if user == None:
     flash('You have to be logged in to access that page!')
@@ -122,12 +168,12 @@ def post_server():
       flash(f"{k}: {v}")
   return redirect('/server/')
 
-@app.get('/terminal/<int:serverId>')
-def get_terminal(serverId):
-  if serverId == None:
+@app.get('/terminal/<int:server_id>')
+def get_terminal(server_id):
+  if server_id == None:
     return redirect('400.html', 400)
-  # Assumes it exists, which is bad
-  server = Server.query.filter_by(id=serverId).first()
+  # TODO: Don't assume it exists, bad
+  server = Server.query.filter_by(id=server_id).first()
   
   return render_template('terminal.html', docker_id=server.docker_id)
 
