ae9cc2c762a85293109a96275bc169bcb8aeb062
Relicjamin-jv <campbellcl19@gcc.edu>
2022-12-22 15:36:08 -0500

    Added commits for testing docker

--- src/tests/test_mcdocker.py
+++ src/tests/test_mcdocker.py
@@ -1,9 +1,14 @@
 import mc_lib.mcdocker as mcdocker
 
 def test_mcdocker():
+    """Makes sure that the docker mc server can still be deployed"""
     server = mcdocker.make_server(threaded=False)
     assert server is not None
 
 def test_change_server_props():
+    """Makes sure that the server config can be changed after creation"""
     assert mcdocker.update_server_properties(updated_properties={"difficulty":"hard"}) == True
 
+def test_rcon():
+    """RCON command to mc-server are configured correctly"""
+    pass
