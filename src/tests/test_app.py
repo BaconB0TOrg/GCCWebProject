fdd22079b438b6298e4c5e981c62f42974f1087d
BaconB0T <63115990+BaconB0T@users.noreply.github.com>
2023-01-04 10:20:48 -0800

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,3 +1,45 @@
-def test_request_example(client):
+from flask import session
+
+####### GET ROUTES #############
+def test_redirect_to_welcome(client):
+    response = client.get("/", follow_redirects=True)
+    assert len(response.history) == 1
+
+def test_welcome(client):
     response = client.get("/welcome/")
-    assert b"<h2>Featured Servers</h2>" in response.data
+    assert b'<h1 class="display-3">Minecraft Server Hosting</h1>' in response.data
+
+def test_get_logout(client):
+    with client:
+        response = client.get("/logout/")
+        assert len(response.history) == 1
+        assert session['logged-in'] == False
+        assert session['user-email'] == None
+        assert session['user-id'] == None
+        assert b'<h1 class="display-3">Minecraft Server Hosting</h1>' in response.data
+    with client.session_transaction() as session:
+        session['user-id'] = 1
+        session['logged-in'] = True
+        session['user-email'] = 
+
+def test_get_login(client):
+    with client:
+        response = client.get("/login/")
+        assert session['logged-in'] != False
+        assert session['user-email'] != None
+        assert session['user-id'] != None
+        assert b'Log in' in response.data
+
+def test_get_register(client):
+    response = client.get("/register/")
+    assert b'Sign up' in response.data
+
+
+####### POST ROUTES #############
+
+def test_post_a_route(client):
+    data = {
+        'field1': 'data1'
+    }
+    response = client.post('/a/', data=data)
+    assert response.status_code == 200
