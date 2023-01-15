df0b2c1a72226b10f5149ea083fddf2f44ac1c2b
Relicjamin-jv <campbellcl19@gcc.edu>
2023-01-15 13:07:26 -0500

    your server page implemented

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
+from forms import LoginForm, RegisterForm, ServerForm, ChangeEmailForm, ServerUpdateForm, CreateServerForm
 import mc_lib.mcdocker as mcdocker
-import os, models
+import os
+import models
 from flask import Flask, escape
 from flask import render_template, url_for, redirect, flash, jsonify, request, session
 from flask_sqlalchemy import SQLAlchemy
@@ -8,7 +10,6 @@ from passlib.hash import sha256_crypt
 scriptdir = os.path.abspath(os.path.dirname(__file__))
 dbpath = os.path.join(scriptdir, 'server_hosting.sqlite3')
 
-from forms import LoginForm, RegisterForm, ServerForm, ChangeEmailForm, ServerUpdateForm, CreateServerForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'somesecretkeythatislongenoughcool'
@@ -21,13 +22,15 @@ db = SQLAlchemy(app)
 # Utilities             #
 #########################
 
+
 def flash_form_errors(form):
-  for field,error in form.errors.items():
+  for field, error in form.errors.items():
     flash(f'{field}: {error}')
 
 #########################
 # Route Handlers        #
 #########################
+
 
 @app.route('/site-map/')
 def site_map():
@@ -40,9 +43,11 @@ def site_map():
   linksString = '<br>'.join(links)
   return linksString
 
+
 @app.get('/')
 def redirect_to_welcome():
   return redirect('/welcome/')
+
 
 @app.get('/welcome/')
 def welcome():
@@ -50,7 +55,7 @@ def welcome():
   users = []
   servers = []
   user = None
-  if(user_id != None):
+  if (user_id != None):
     users = User.query.filter_by(id=user_id).all()
     if len(users) != 0:
       servers = Server.query.filter_by(owner_id=user_id).all()
@@ -60,6 +65,7 @@ def welcome():
 
   return render_template('home.html', user=user)
 
+
 @app.get('/login/')
 def get_login():
   if session.get('logged-in'):
@@ -67,6 +73,7 @@ def get_login():
     return redirect(url_for('welcome'))
   form = LoginForm()
   return render_template('login.html', form=form)
+
 
 @app.post('/login/')
 def post_login():
@@ -105,10 +112,12 @@ def post_login():
     flash_form_errors(form)
     return redirect(url_for('get_login'))
 
+
 @app.route('/logout/')
 def get_log_out():
   if request.method != 'GET':
-    print(f"[WARN] User tried to {request.method} to {url_for('get_log_out')}. Only GET is allowed")
+    print(
+        f"[WARN] User tried to {request.method} to {url_for('get_log_out')}. Only GET is allowed")
     flash("You can't do that!")
     return redirect(url_for('welcome'))
   # if you're not logged in, wut
@@ -123,6 +132,7 @@ def get_log_out():
   flash("You've been logged out.")
   return redirect(url_for('welcome'))
 
+
 @app.get('/account/')
 def get_account():
   # if you're not logged in, you can't be here.
@@ -135,13 +145,16 @@ def get_account():
   user = User.query.filter_by(id=user_id).first()
   # if user doesn't exist
   if not user:
-    print(f"[WARN] Failed to GET {url_for('get_account')}: user {user_id} doesn't exist")
+    print(
+        f"[WARN] Failed to GET {url_for('get_account')}: user {user_id} doesn't exist")
     flash('You have to be logged in to see that page.')
     return redirect(url_for('get_login'))
-  
+
   form = ChangeEmailForm()
-  print(f"[INFO] Successfully accessed {url_for('get_account')} for user {user_id}")
+  print(
+      f"[INFO] Successfully accessed {url_for('get_account')} for user {user_id}")
   return render_template('account.html', user=user, form=form)
+
 
 @app.post('/account/change-email/')
 def change_email():
@@ -166,10 +179,12 @@ def change_email():
     flash_form_errors(form)
     return redirect(url_for('get_account'))
 
+
 @app.get('/register/')
 def get_register():
     form = RegisterForm()
     return render_template('register.html', form=form)
+
 
 @app.post('/register/')
 def post_register():
@@ -203,13 +218,14 @@ def post_register():
 
         return redirect(url_for('welcome'))
     else:
-        # flash error messages for all validation problems 
+        # flash error messages for all validation problems
         flash_form_errors(form)
         return redirect(url_for('get_register'))
 
+
 @app.get('/servers/')
 def list_server():
-  email=session.get('user-email')
+  email = session.get('user-email')
   user = User.query.filter_by(email=email).first()
   user_id = None
   if user:
@@ -217,6 +233,7 @@ def list_server():
   servers = Server.query.all()
   # servers = Server.query.filter_by(owner_id=user.id).all()
   return render_template('server_list.html', servers=servers, user_id=user_id)
+
 
 @app.get('/servers/create/')
 def get_create_server():
@@ -229,10 +246,12 @@ def get_create_server():
   form.tags.choices = [(t.id, t.name) for t in tags]
   return render_template('server_creation.html', form=form)
 
+
 @app.post('/servers/create/')
 def post_create_server():
   if not session.get('logged-in'):
-    print(f'[INFO] Anonymous user tried to {request.method} {url_for("post_create_server")}')
+    print(
+        f'[INFO] Anonymous user tried to {request.method} {url_for("post_create_server")}')
     flash("You must be logged in to do that!")
     return redirect(url_for('get_login'))
 
@@ -251,15 +270,17 @@ def post_create_server():
     # this calc won't work if we delete servers who no longer have docker containers from the db.
     port = 25565+Server.query.count()*2
     # TODO: Pass properties here rather than force a separate call.
-    docker_id = mcdocker.make_server(name=form.server_name.data,port=port, max_players=str(form.number_of_players.data), gamemode=str(form.gamemode.data.lower()))
-    
+    docker_id = mcdocker.make_server(name=form.server_name.data, port=port, max_players=str(
+        form.number_of_players.data), gamemode=str(form.gamemode.data.lower()))
+
     if not docker_id:
       print("[CRITICAL] Server could not be created!")
       flash('The server could not be created, please wait before trying again.')
       return redirect(url_for('get_create_server'))
 
     tags = Tag.query.filter(Tag.id.in_(form.tags.data)).all()
-    server = Server(name=form.server_name.data, description=str(form.server_description.data), docker_id=str(docker_id), owner_id=user.id, max_players=int(form.number_of_players.data), port=port, tags=tags)
+    server = Server(name=form.server_name.data, description=str(form.server_description.data), docker_id=str(
+        docker_id), owner_id=user.id, max_players=int(form.number_of_players.data), port=port, tags=tags)
 
     db.session.add(server)
     db.session.commit()
@@ -268,6 +289,7 @@ def post_create_server():
   else:
     flash_form_errors(form)
     return redirect(url_for('get_create_server'))
+
 
 @app.get('/servers/<int:server_id>/')
 def show_server(server_id):
@@ -285,10 +307,12 @@ def show_server(server_id):
 
   return render_template('server.html', server=server)
 
+
 @app.get('/servers/<int:server_id>/update/')
 def get_update_server(server_id):
   if not session.get('logged-in'):
-    print(f'[INFO] Anonymous user tried to {request.method} {url_for("get_update_server")}')
+    print(
+        f'[INFO] Anonymous user tried to {request.method} {url_for("get_update_server")}')
     flash("You must be logged in to do that!")
     return redirect(url_for('get_login'))
 
@@ -299,13 +323,14 @@ def get_update_server(server_id):
     print(f"[ERROR] Failed to update server: User {user_id} does not exist")
     flash("You don't have permission to do that!")
     return redirect(url_for('get_login'))
-  
+
   server = Server.query.filter_by(id=server_id).first()
   if not server or server.owner_id != user_id:
     if not server:
       print(f'[WARN] Server {server_id} does not exist.')
     else:
-      print(f'[WARN] User {user_id} tried updating Server {server_id}, but does not own it.')
+      print(
+          f'[WARN] User {user_id} tried updating Server {server_id}, but does not own it.')
     flash("You don't have permission to do that!")
     return redirect(url_for('list_server'))
   # user exists and is logged in, server exists and is owned by said user.
@@ -320,10 +345,12 @@ def get_update_server(server_id):
   form.tags.choices = [(t.id, t.name) for t in tags]
   return render_template('update_server.html', form=form)
 
+
 @app.post('/servers/<int:server_id>/update/')
 def post_update_server(server_id):
   if not session.get('logged-in'):
-    print(f'[INFO] Anonymous user tried to {request.method} {url_for("post_update_server")}')
+    print(
+        f'[INFO] Anonymous user tried to {request.method} {url_for("post_update_server")}')
     flash("You must be logged in to do that!")
     return redirect(url_for('get_login'))
 
@@ -348,10 +375,11 @@ def post_update_server(server_id):
       print(f"[WARN] Failed to update server: User doesn't own any servers!")
       flash("You have to own the server in order to update it!")
       return redirect(url_for('get_create_server'))
-    
-    server_as_list = list(filter(lambda s : s.id == form_server_id, servers))
+
+    server_as_list = list(filter(lambda s: s.id == form_server_id, servers))
     if not server_as_list or len(server_as_list) == 0:
-      print(f'[WARN] Failed to update server: User {user_id} does not own Server {server_id}!')
+      print(
+          f'[WARN] Failed to update server: User {user_id} does not own Server {server_id}!')
       flash("You have to own the server in order to update it!")
       return redirect(url_for('get_create_server'))
 
@@ -369,13 +397,14 @@ def post_update_server(server_id):
     flash_form_errors(form)
     return redirect(url_for('get_update_server'))
 
+
 @app.get('/servers/<int:server_id>/delete')
 def delete_server(server_id):
   if not session.get('logged-in'):
     print(f'Anonymous user tried to delete server {server_id}')
     flash("You don't have permission to do that!")
     return redirect(url_for('welcome'))
-  
+
   server = Server.query.filter_by(id=server_id).first()
   if session.get('user-id') == server.owner_id:
     mcdocker.remove_docker(container_id=server.docker_id)
@@ -384,9 +413,11 @@ def delete_server(server_id):
     flash('Server succesfully deleted!')
     return redirect(url_for('list_server'))
   else:
-    print(f'[WARN] User {session.get("user-id")} tried to delete server {server_id}')
+    print(
+        f'[WARN] User {session.get("user-id")} tried to delete server {server_id}')
     flash("You don't have permission to do that!")
     return redirect(url_for('list_server'))
+
 
 @app.get('/servers/<int:server_id>/terminal/')
 def get_terminal(server_id):
@@ -395,20 +426,26 @@ def get_terminal(server_id):
   server = Server.query.filter_by(id=server_id).first()
   if not server:
     print(f'Failed to access server {server_id}: Does not exist')
-    flash("You don't have permission to do that!")  
+    flash("You don't have permission to do that!")
     return redirect(url_for('welcome'))
   # TODO: Check userServerRole for permission to access Terminal
   if server.owner_id != int(session.get('user-id')):
-    print(f'User {session.get("user-id")} failed to access server {server_id}: Not the owner')
-    flash("You don't have permission to do that!") 
+    print(
+        f'User {session.get("user-id")} failed to access server {server_id}: Not the owner')
+    flash("You don't have permission to do that!")
     return redirect(url_for('welcome'))
-  
+
   return render_template('terminal.html', docker_id=server.docker_id)
 
-@app.get("/users/servers/")
-def get_users_servers():
 
-  return render_template('users_servers.html')
+@app.get("/users/<int:user_id>/servers/")
+def get_users_servers(user_id):
+  if user_id == None:
+    return redirect('400.html', 400)
+
+  users_server = Server.query.filter_by(owner_id=user_id).all()
+
+  return render_template('users_servers.html', servers=users_server)
 
 @app.get('/mc_command/')
 def mc_command():
